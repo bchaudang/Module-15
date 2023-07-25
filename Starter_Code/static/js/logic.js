397d8d0185f2bdeb327d75cb6b797760f307c815
Brettney Chau-Dang <bchaudang@gmail.com>
2023-07-25 16:26:29 -0400

    colour markers

--- Starter_Code/static/js/logic.js
+++ Starter_Code/static/js/logic.js
@@ -1,35 +1,94 @@
-// map
-var myMap = L.map("map", {
-    center: [45.42, -75.69],
-    zoom: 2
+// Store our API endpoint as queryUrl.
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+// Perform a GET request to the query URL/
+d3.json(queryUrl).then(function (data) {
+  console.log(data);
+  // Once we get a response, send the data.features object to the createFeatures function.
+  createFeatures(data.features);
   });
+
+// Marker sizes
+function markerSize(magnitude) {
+  return magnitude * 4;
+};
+
+// Change marker colours depending on depth
+function chooseColour(depth) {
+  if (depth < 20) return "#d4ff52";
+  else if (depth < 40) return "#ffff52";
+  else if (depth < 60) return "#ffd452";
+  else if (depth < 80) return "#ffa852";
+  else if (depth < 100) return "#ff7d52";
+  else return "#ff5050";
+}
+
+  function createFeatures(earthquakeData) {
+
+    // Define a function that we want to run once for each feature in the features array.
+    // Give each feature a popup that describes the place and time of the earthquake.
+    function onEachFeature(feature, layer) {
+      layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p><p>Magnitude: ${feature.properties.mag}</p><p>Depth: ${feature.geometry.coordinates[2]}</p>`);
+    }
   
-  // Adding the tile layer
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    // Create a GeoJSON layer that contains the features array on the earthquakeData object.
+    // Run the onEachFeature function once for each piece of data in the array.
+    var earthquakes = L.geoJSON(earthquakeData, {
+      onEachFeature: onEachFeature,
+
+      pointToLayer: function(feature, latlng) {
+
+        var markers = {
+          radius: markerSize(feature.properties.mag),
+          color: chooseColour(feature.geometry.coordinates[2]),
+          fillOpacity: 0.5,
+          stroke: true,
+          weight: 20,
+        }
+        return L.circle(latlng, markers);
+      }
+    });
+  
+    // Send our earthquakes layer to the createMap function/
+    createMap(earthquakes);
+  }
+  
+  function createMap(earthquakes) {
+  
+    // Create the base layers.  
+    var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(myMap);
+    })
   
-  var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+    var topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+      attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+    });
   
-  d3.json(url).then(function(response) {
-
-    //console.log(response);
-    features = response.features;
+    // Create a baseMaps object.
+    var baseMaps = {
+      "Street Map": street,
+      "Topographic Map": topo
+    };
   
-    //console.log(features);
+    // Create an overlay object to hold our overlay.
+    var overlayMaps = {
+      Earthquakes: earthquakes
+    };
   
-    for (var i = 0; i < features.length; i++) {
+    // Create our map, giving it the streetmap and earthquakes layers to display on load.
+    var myMap = L.map("map", {
+      center: [
+        37.09, -95.71
+      ],
+      zoom: 5,
+      layers: [street, earthquakes]
+    });
   
-      var location = features[i].geometry;
-      if(location){
-        L.circle([location.coordinates[1], location.coordinates[0]], {
-            color: "green",
-            fillColor: "green",
-            fillOpacity: 0.75,
-            radius: 500
-          }).addTo(myMap);
-        }}   
-
-  
-  });
+    // Create a layer control.
+    // Pass it our baseMaps and overlayMaps.
+    // Add the layer control to the map.
+    L.control.layers(baseMaps, overlayMaps, {
+      collapsed: false
+    }).addTo(myMap);
+  };
   
