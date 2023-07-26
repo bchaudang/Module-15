d8051869e53b79262ebdb0081134c145d4ad7816
Brettney Chau-Dang <bchaudang@gmail.com>
2023-07-25 21:53:00 -0400

    div box test

--- Starter_Code/static/js/logic.js
+++ Starter_Code/static/js/logic.js
@@ -23,12 +23,28 @@ function chooseColour(depth) {
   else return "#ff5050";
 }
 
+
+  // Add the legend with colors to corrolate with depth
+  var legend = L.control({position: "bottomright"});
+  legend.onAdd = function() {
+    var div = L.DomUtil.create("div", "info legend"),
+    depth = [-10, 10, 30, 50, 70, 90];
+  
+    for (var i = 0; i < depth.length; i++) {
+      div.innerHTML +=
+      '<i style="background:' + chooseColour(depth[i] + 1) + '"></i> ' + depth[i] + (depth[i + 1] ? '&ndash;' + depth[i + 1] + '<br>' : '+');
+    }
+    return div;
+  };
+  legend.addTo(myMap);
+  
+
   function createFeatures(earthquakeData) {
 
     // Define a function that we want to run once for each feature in the features array.
     // Give each feature a popup that describes the place and time of the earthquake.
     function onEachFeature(feature, layer) {
-      layer.bindPopup("<h2>${feature.properties.place}</h2><hr><p>${new Date(feature.properties.time)}</p><p>Magnitude: ${feature.properties.mag}</p><p>Depth: ${feature.geometry.coordinates[2]}</p>");
+      layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p><p>Magnitude: ${feature.properties.mag}</p><p>Depth: ${feature.geometry.coordinates[2]}</p>`);
     }
       // Create a GeoJSON layer that contains the features array on the earthquakeData object.
     // Run the onEachFeature function once for each piece of data in the array.
@@ -40,7 +56,7 @@ function chooseColour(depth) {
         var markerOptions = {
           radius: markerSize(feature.properties.mag),
           color: chooseColour(feature.geometry.coordinates[2]),
-          opacity: 2,
+          opacity: 3,
           stroke: true,
           weight: 2,
         } 
@@ -89,6 +105,6 @@ function chooseColour(depth) {
     L.control.layers(baseMaps, overlayMaps, {
       collapsed: false
     }).addTo(myMap);
-
+    legend.addTo(myMap);
   };
-  
+
